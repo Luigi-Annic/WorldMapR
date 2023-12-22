68b0c767623b63ea7672ff025207187a86995cc8
Luigi-Annic <luigi.annic@gmail.com>
2023-12-22 11:25:12 +0100

    test data set included

---
+++ R/testdata1.R
@@ -0,0 +1,10 @@
+#' Simulated data set 1
+#'
+#' Data from a random simulation with continuous data, for testing the functions
+#'
+#' @docType data
+#'
+#' @usage data(testdata1)
+#'
+#' @format An object of class \code{"data.frame"}
+#' @keywords datasets
