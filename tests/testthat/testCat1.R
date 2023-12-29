a3ec35197652f2db51dc1753c1ed9ddd846ae782
Luigi-Annic <luigi.annic@gmail.com>
2023-12-29 09:50:29 +0100

    tests for categorical data added

---
+++ tests/testthat/testCat1.R
@@ -0,0 +1,6 @@
+worldplotCat(simdata = testdata1b,
+             div = 1,
+             ColName = "VCat",
+             CountryName = "Cshort",
+             CountryNameType = "isoa2",
+             annote = FALSE)
